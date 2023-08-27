b1f41bb1a4b01151ad3d120db3301e73df997d84
Daniel Moraes <96148567+SirDnx@users.noreply.github.com>
2023-08-27 14:05:25 -0300

    more studies

---
+++ tab-menu-navigation/assets/script.js
@@ -0,0 +1,11 @@
+let list = document.querySelectorAll('.list');
+    
+    list.forEach((item) => {
+        item.addEventListener('click', activeLink);
+    })
+
+    function activeLink(){
+        list.forEach((item) => 
+        item.classList.remove('active'));
+        this.classList.add('active');
+}
